80acdd0ece2ef3dad640202e10e665579ffe3b54
Parkjinsil <133620283+Parkjinsil@users.noreply.github.com>
2023-05-16 21:41:15 +0900

    Update CastingPractice.java

--- 02_Variable/src/com/kh/practice/CastingPractice.java
+++ 02_Variable/src/com/kh/practice/CastingPractice.java
@@ -9,19 +9,41 @@ public class CastingPractice {
 		CastingPractice cp = new CastingPractice();
 //		cp.method1();
 		cp.method2();
-//		cp.method3();
 	}
 	
 	public void method1() {
 
+            Scanner sc = new Scanner(System.in);
+		
+            System.out.print("문자 : ");
+	    String st = sc.nextLine();
+	    
+	    char ch = st.charAt(0);
+	    int a = ch;
+	    
+	    System.out.println("A unicode : "+a);
+	    System.out.println("B unicode : "+(a+1));
 		
 	}
 	
 	public void method2() {
 		
-		Scanner sc = new Scanner(System.in);
+	    Scanner sc = new Scanner(System.in);
 		
-		System.out.println();
+	    System.out.println("국어 : ");
+	    int num1 = sc.nextInt();
+	    
+	    System.out.println("영어 : ");
+	    int num2 = sc.nextInt();
+	    
+	    System.out.println("수학 : ");
+	    int num3 = sc.nextInt();
+	    
+	    float aver = (num1 + num2 + num3)/3F;
+	    
+	    System.out.println("총점 : "+(num1 + num2 + num3));
+	    System.out.printf("평균 : %.2f", aver);
+	
 		
 	}
 	
