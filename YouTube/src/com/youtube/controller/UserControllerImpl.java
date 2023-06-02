4307d3918921561853929cf5f2e2ee9aef75f099
박진실 <wlstlf971017@naver.com>
2023-06-02 13:53:33 +0900

    10

--- YouTube/src/com/youtube/controller/UserControllerImpl.java
+++ YouTube/src/com/youtube/controller/UserControllerImpl.java
@@ -5,9 +5,9 @@ import com.youtube.model.User;
 public interface UserControllerImpl {
 
 	public boolean login();         // 로그인 
-	public boolean signUp();        // 회원가입
+	public void signUp();        // 회원가입
 	public User viewProfile();      // 프로필 보기
 	public User updateProfile();    // 프로필 수정
-	public boolean deleteProfile(); // 계정 삭제
+	public void deleteProfile(); // 계정 삭제
 	
 }
