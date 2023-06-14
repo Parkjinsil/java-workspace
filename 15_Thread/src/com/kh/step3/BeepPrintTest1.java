de79a14dfb9c6ce689e80d39d4a6e96f55c3fd67
박진실 <wlstlf971017@naver.com>
2023-06-14 14:00:19 +0900

    15

---
+++ 15_Thread/src/com/kh/step3/BeepPrintTest1.java
@@ -0,0 +1,26 @@
+package com.kh.step3;
+
+import java.awt.Toolkit;
+
+public class BeepPrintTest1 {
+
+	public static void main(String[] args) {
+
+		Toolkit tool = Toolkit.getDefaultToolkit();
+		for(int i=0; i<5; i++) {
+			tool.beep();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {}
+		}
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("띵~");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {}
+		}
+		
+	}
+
+}
