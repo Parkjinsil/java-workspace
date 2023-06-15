56ed628c1104071580a7e8e0ac3991f261fba080
Parkjinsil <wlstlf971017@naver.com>
2023-06-15 19:57:38 +0900

    16

--- 15_Thread/src/com/kh/step5/User2.java
+++ 15_Thread/src/com/kh/step5/User2.java
@@ -4,16 +4,12 @@ public class User2 extends Thread {
 	
 	private Calculator calculator;
 	
-	public void setCalculator(Calculator calculator) { // 매개변수(갈색)로 가지고 온 걸 파란색에 넣기
+	public void setCalculator(Calculator calculator) {
 		this.calculator = calculator;
 		setName("CalculatorUser2");
 	}
 	
 	public void run() {
 		calculator.setMemory(50);
-		
 	}
-	
-	
-
 }
