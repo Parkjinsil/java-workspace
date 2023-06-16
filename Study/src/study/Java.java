b2085020443f9011574d4e6efa300609d37d8969
박진실 <wlstlf971017@naver.com>
2023-06-16 17:42:58 +0900

    6/16

--- Study/src/study/Java.java
+++ Study/src/study/Java.java
@@ -1,12 +1,16 @@
 package study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Java {
 	
 	/*
 	 *  <11.Collection> -> 인터페이스
 	 *  컬렉션 : 여러원소들을 담을 수 있는 자료구조 (동적 메모리 할당)
+	 *         컬렉션 사용하면 데이터 추가 삭제 정렬 등의 처리가 간단하게 해결됨
 	 *        - list   : 순서 o 중복 o  (ArrayList = 클래스)
-	 *        - set    : 순서 중요 x
+	 *        - set    : 순서 x 중복 x
 	 *        - queue  : FIFO (fist in first out) 
 	 *        - map    : key-value 형태
 	 *   
@@ -17,7 +21,7 @@ public class Java {
 	 *  메소드
 	 *  add(E e) : 끝에 추가
 	 *  add(int index, E e) : 해당 인덱스에 추가
-	 *  addall(Collection c) : 컬렉션 통채로 뒤에 추가
+	 *  addAll(Collection c) : 컬렉션 통채로 뒤에 추가
 	 *  set(int index, E e) : 해당 인덱스 값변경
 	 *  get(int index) : 해당 인덱스 객체 가져오기
 	 *  subList(int index1, int index2) : 추출해서 새로운 list
@@ -31,19 +35,43 @@ public class Java {
 	 *  remove(int index) : 해당 인덱스 객체 삭제
 	 *  size() : 데이터 개수
 	 *  toArray() : 컬렉션에 있는 데이터 배열로 복사
+	 *  indexOf(Object o) : 해당 객체 인덱스값 반환
 	 *  
-	 *  Map
+	 *  오름차순 정렬(자바에서 제공하는 클래스 아니면 
+	 *            클래스에다가 implements Comparable<Person> 이런식 해줘야함
+	 *  - Collections.sort() 
+	 *  내림차순 정렬 (오름차순 해주고 반대로) 
+	 *  - Collections.reverse() 
+	 *  
+	 *  Map (put, remove)
 	 *  - 값만 저장x
 	 *  - 키만 저장o
 	 *  - 키는 중복x
 	 *  - 값은 중복o
 	 *  
-	 *  -> HashMap
-	 *  -> TreeMap :키와 값을 저장하는 동시에 정렬 
+	 *  -> HashMap : Map 인터페이스를 구현한 대표적인 컬렉션 클래스
+	 *  -> TreeMap : 키와 값을 저장하는 동시에 정렬 
 	 *  
+	 *  key 가져오기 : keySet() 
+	 *  value 가져오기 : values()
+	 *  key에 해당하는 value 가져오기
+	 *    - Iterator<키의 자료형> it = (컬렉션의 키리스트).iterator();
+	 *    - 다음 요소가 있는지 판단하는 .hasNext()
+	 *    - 다음 요소를 가져오는 .next()
+	 *    - 가져온 요소를 삭제하는 .remove()
+	 *    - (리스트).get(키) -> 키의 값 가져옴 
+	 *    
+	 *  Set
+	 *  -> HashSet : Set 인터페이스를 구현한 대표적인 컬렉션 클래스
 	 *  
 	 *  
 	 */
+	
+	public static void main(String[] args) {
+
+		
+	
+	}
 	
 }
 
