bae461a34eee6a5edd355e8cd9169ece22ae5b85
Parkjinsil <133620283+Parkjinsil@users.noreply.github.com>
2023-06-17 17:35:02 +0900

    15

--- 10_Exception/src/com/kh/practice1/A_TryCatch.java
+++ 10_Exception/src/com/kh/practice1/A_TryCatch.java
@@ -28,11 +28,7 @@ public class A_TryCatch {
 	 * 	// finally 주로 자원을 반납하는 코드...close()
 	 * }]
 	 * 
-<<<<<<< HEAD
-	 * - printStackTrace() : 예외발생 당시의 호출스택(Call stack에 있었던 메소드의 정보와 예외 메세지를 화면에 출력)
-=======
 	 * - printStackTrace() : 예외발생 당시의 호출스택(Call stack에 있었던 메소드의 정보와 예외 메세지)를 화면에 출력
->>>>>>> b2085020443f9011574d4e6efa300609d37d8969
 	 * - getMessage() : 발생한 예외클래스의 객체에 저장된 메세지를 얻을 수 있음
 	 */
 	public static void main(String[] args) {
